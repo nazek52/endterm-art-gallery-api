16aa66005d60fe8b6203da7b6fc839a40d9b2d06
Rymbek Nazerke <nazerkerymbek3@gmail.com>
2026-02-10 23:02:34 +0500

    Create ArtworkService.java

---
+++ src/main/java/com/example/artgallery/service/ArtworkService.java
@@ -0,0 +1,10 @@
+package com.example.artgallery.service;
+
+import com.example.artgallery.model.Artwork;
+
+import java.util.List;
+
+public interface ArtworkService {
+
+    List<Artwork> getAllArtworks();
+}
