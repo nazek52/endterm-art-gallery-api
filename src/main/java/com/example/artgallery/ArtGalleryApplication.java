23033522ae6075345da21d14253a6317d14b01d9
Rymbek Nazerke <nazerkerymbek3@gmail.com>
2026-02-10 22:59:17 +0500

    Create ArtGalleryApplication.java

---
+++ src/main/java/com/example/artgallery/ArtGalleryApplication.java
@@ -0,0 +1,12 @@
+package com.example.artgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtGalleryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtGalleryApplication.class, args);
+    }
+}
