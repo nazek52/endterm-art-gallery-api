7b32c0095cdd32bc5b7c98021e5e7b2a6af1aed0
Rymbek Nazerke <nazerkerymbek3@gmail.com>
2026-02-10 23:19:40 +0500

    Update ArtworkServiceImpl.java

--- src/main/java/com/example/artgallery/service/impl/ArtworkServiceImpl.java
+++ src/main/java/com/example/artgallery/service/impl/ArtworkServiceImpl.java
@@ -21,22 +21,22 @@ public class ArtworkServiceImpl implements ArtworkService {
     @PostConstruct
     public void init() {
         repository.save(new Painting(1, "Starry Night", 1_000_000,
-                new Artist(1, "Vincent van Gogh\n")));
+                new Artist(1, "Vincent van Gogh")));
 
         repository.save(new Painting(2, "Mona Lisa", 850_000,
-                new Artist(2, "Leonardo da Vinci\n")));
+                new Artist(2, "Leonardo da Vinci")));
 
         repository.save(new Sculpture(3, "The Thinker", 300_000,
-                new Artist(3, "Auguste Rodin\n")));
+                new Artist(3, "Auguste Rodin")));
 
         repository.save(new Sculpture(4, "Venus de Milo", 400_000,
-                new Artist(4, "Alexandros of Antioch\n")));
+                new Artist(4, "Alexandros of Antioch")));
 
         repository.save(new Painting(5, "The Scream", 900_000,
-                new Artist(5, "Edvard Munch\n")));
+                new Artist(5, "Edvard Munch")));
 
         repository.save(new Sculpture(6, "David", 500_000,
-                new Artist(6, "Michelangelo\n")));
+                new Artist(6, "Michelangelo")));
     }
 
     @Override
