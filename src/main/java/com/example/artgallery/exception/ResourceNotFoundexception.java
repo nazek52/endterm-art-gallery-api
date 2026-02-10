bca89fa6784d811f94600ff429f7dccbb7e733fb
Rymbek Nazerke <nazerkerymbek3@gmail.com>
2026-02-10 23:14:17 +0500

    Create ResourceNotFoundexception.java

---
+++ src/main/java/com/example/artgallery/exception/ResourceNotFoundexception.java
@@ -0,0 +1,7 @@
+package com.example.artgallery.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
