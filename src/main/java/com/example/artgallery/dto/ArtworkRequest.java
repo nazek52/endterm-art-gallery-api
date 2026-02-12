7560e237174ce96740ae10a34ba8428043bc94fa
Rymbek Nazerke <nazerkerymbek3@gmail.com>
2026-02-12 17:36:02 +0500

    Update ArtworkRequest.java

--- src/main/java/com/example/artgallery/dto/ArtworkRequest.java
+++ src/main/java/com/example/artgallery/dto/ArtworkRequest.java
@@ -1,7 +1,23 @@
 package com.example.artgallery.dto;
 
 public class ArtworkRequest {
-    public int id;
+
     public String title;
     public double price;
+    public String type;
+
+    public ArtworkRequest() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
